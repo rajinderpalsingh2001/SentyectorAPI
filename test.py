bd34399da70fc675b706eed1c5233a55f3ff0c3d
sairish2001 <sairish2001@gmail.com>
2021-04-10 17:03:40 +0530

    README added

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import requests
 import json
 
 # ---for prediciton---
-url = 'https://sentimentsapipractice.herokuapp.com/predictsenitments/'
+url = 'https://sentyectorapi.herokuapp.com/api/predemotions/'
 data = {'textopredict': "You're just too good to be true can't take my eyes off you you'd be like heaven to touch I wanna hold you so much I love you baby"}
 data = json.dumps(data)
 r = requests.post(url, data = data)
 print(r.text)
 
 # ---for audiototext---
-url = 'https://sentimentsapipractice.herokuapp.com/audiototext/'
+url = 'https://sentyectorapi.herokuapp.com/api/audiochunktotext/'
 files={'file':open(r'C:\Users\Sairish\Downloads\myrecord.wav','rb')}
 r = requests.post(url, files = files)
 print(r.text)
